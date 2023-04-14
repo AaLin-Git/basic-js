77ddf4b0c626fffe9abc3768835dce432c2146f2
AaLin-Git <aalin.box.it@gmail.com>
2023-04-14 16:37:57 +0200

    feat: write function for count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  //throw new NotImplementedError('Not implemented');
+  let count = 0;
+  function openMatrix(arr) {
+    arr.forEach((item) => {
+      if (item === "^^" && item.length === 2) {
+        count++;
+      } else if (typeof item === "object") {
+        openMatrix(item);
+      }
+    });
+  }
+  openMatrix(matrix);
+  return count;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
