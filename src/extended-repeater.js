2f9b7016db8d344008a1de6e33bd41c33830ea12
AaLin-Git <aalin.box.it@gmail.com>
2023-04-17 18:36:00 +0200

    feat: writw function for extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,41 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newStr = "";
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+  for (let i = 0; i < repeatTimes; i++) {
+    newStr += str;
+    for (let x = 0; x < additionRepeatTimes; x++) {
+      newStr += addition;
+      if (x !== additionRepeatTimes - 1) {
+        newStr += additionSeparator;
+      }
+    }
+    if (i !== repeatTimes - 1) {
+      newStr += separator;
+    }
+  }
+  return newStr;
 }
 
 module.exports = {
